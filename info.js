beb7f9683a869803a0956d7c9c62ecdb1199b28e
levure-s <levure.sauvage@gmail.com>
2024-08-01 07:33:05 +0900

    first-commit

---
+++ info.js
@@ -0,0 +1,70 @@
+const tweets = {
+  Ageha: {
+    img: "Ageha.jpg",
+    url: "https://x.com/ageha_Babys/status/1818663131708899487",
+  },
+  Yuna: {
+    img: "Yuna.jpg",
+    url: "https://x.com/KnwRwi/status/1818662906973839692",
+  },
+  Karin: {
+    img: "Karin.jpg",
+    url: "https://x.com/t0n0_k6r1n/status/1818664598352048535",
+  },
+  Haruki: {
+    img: "Haruki.jpg",
+    url: "https://x.com/haru4869_Babys/status/1818664276804223412",
+  },
+  Hassum: {
+    img: "Hassum.jpg",
+    url: "https://x.com/nado_naru/status/1818672459044020696",
+  },
+  Erika: {
+    img: "Erika.jpg",
+    url: "https://x.com/C45iv1nRVAH9Ox4/status/1818664222689313269",
+  },
+  Kentaro: {
+    img: "Kentaro.jpg",
+    url: "https://x.com/ken_Babys1112/status/1818667773981307165",
+  },
+  Aiiro: {
+    img: "Aiiro.jpg",
+    url: "https://x.com/aiiro_babys/status/1818666593972355141",
+  },
+  Tantan: {
+    img: "Tantanmen.jpg",
+    url: "https://x.com/tan_tan_men1024/status/1818662906638352424",
+  },
+  Ayu: {
+    img: "Ayu.jpg",
+    url: "https://x.com/chocolaterusk3/status/1818746450211377214",
+  },
+  Chiya: {
+    img: "Chiya.jpg",
+    url: "https://x.com/chiya__k2305/status/1818689109843755045",
+  },
+  Nanami: {
+    img: "Nanami.jpg",
+    url: "https://x.com/nanami_Babys/status/1818662921607733277",
+  },
+  Onesama: {
+    img: "Onesama.jpg",
+    url: "https://x.com/417_cl0vers/status/1818663091472941080",
+  },
+  Fuuka: {
+    img: "Fuuka.jpg",
+    url: "https://x.com/_Fuuka_MUSIC/status/1818666047928676384",
+  },
+  Ayuka: {
+    img: "Ayuka.jpg",
+    url: "https://x.com/Ayukababys1/status/1818662927047741655",
+  },
+  HARU: {
+    img: "HARUMARU.jpg",
+    url: "https://x.com/HARUMARU730/status/1818755580280160358",
+  },
+  Levure: {
+    img: "Levure.jpg",
+    url: "https://x.com/8J6pxh2o0XB5lN9/status/1818664005222957481",
+  },
+};
